8246d425137598bb465060b3debb36f74ac1cc14
yum1929 <dangodaikazoku22@me.com>
2019-06-12 17:16:12 +0900

    配列に格納

--- jsPsych/programs/img.php
+++ jsPsych/programs/img.php
@@ -1,14 +1,19 @@
 <?php
-// ディレクトリのパス
-$dir = "img/" ;
+	// ディレクトリのパス
+	$dir = "img/" ;
 
-if( is_dir( $dir ) && $handle = opendir( $dir ) ) {
-	while( ($file = readdir($handle)) !== false ) {
-		if( filetype( $path = $dir . $file ) == "file" ) {
-			// $file: ファイル名
-			// $path: ファイルのパス
-			echo file;
+	if( is_dir( $dir ) && $handle = opendir( $dir ) ) {
+		while( ($file = readdir($handle)) !== false ) {
+			if( filetype( $path = $dir . $file ) == "file" ) {
+				// $file: ファイル名
+				// $path: ファイルのパス
+			}
 		}
 	}
-}
+
+	$files = array();
+	foreach(glob($dir . '*png') as $file){
+		array_push($files, $file);
+	}
+	var_dump($files);
 ?>
