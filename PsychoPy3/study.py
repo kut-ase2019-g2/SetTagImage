9ea89f9c5a513571fb2f49132f6481144886cfee
yum1929 <dangodaikazoku22@me.com>
2019-07-06 18:10:39 +0900

    試行数が表示される

--- PsychoPy3/study.py
+++ PsychoPy3/study.py
@@ -50,7 +50,7 @@ try:
 
 この教示が読めたら、「スペース」キーを押して課題を始めてください"""
     instText = visual.TextStim(myWin, text=instructionStr,pos=(0,0), color=(-1,-1,-1), height=0.08)
-    #instText.font = 'ヒラギノ角ゴシック W5'
+    instText.font = 'ヒラギノ角ゴシック W5'
     instText.draw()
     myWin.flip()
     # 参加者がspaceキーを押すまで画面を出したまま待つ。
@@ -61,19 +61,24 @@ try:
 
     for i in range(len(img)):
         tag = [u'キラキラ', u'ハート', u'音符', u'炎', u'集中線', u'波紋', u'電撃', u'レンズフレア', u'漫画風', u'振動', u'コントラスト', u'拡大', u'縮小']
-        for k in range(len(tag)-1):
+        k = len(tag)-1
+        imgN = visual.TextStim(myWin, text=str(i)+'/'+str(len(img)),pos=(0.9,-0.9), color=(-1,-1,-1), height=0.05)
+        while k > 0:
             #print(tag)
             numpy.random.shuffle(tag)
             # テキストの準備
             myText = visual.ImageStim(myWin, image=img[i], pos=(0,0.3), units='norm',size = [1,1])
             leftText = visual.TextStim(myWin, text=str(tag[0]), pos=(-0.5,-0.7), color=(-1,-1,-1), colorSpace='rgb', height=0.2)
-            #leftText.font = 'ヒラギノ角ゴシック W5'
+            leftText.font = 'ヒラギノ角ゴシック W5'
             rightText = visual.TextStim(myWin, text=str(tag[1]), pos=(0.5,-0.7), color=(-1,-1,-1), colorSpace='rgb', height=0.2)
-            #rightText.font = 'ヒラギノ角ゴシック W5'
+            rightText.font = 'ヒラギノ角ゴシック W5'
+            tagN = visual.TextStim(myWin, text=str(len(tag)-1),pos=(0.9,0.9), color=(-1,-1,-1), height=0.05)
             # テキストの書き込み
             myText.draw()
             leftText.draw()
             rightText.draw()
+            imgN.draw()
+            tagN.draw()
             # 提示
             myWin.flip()
 
@@ -82,14 +87,18 @@ try:
             event.clearEvents()
             # 参加者の反応をリセット
             Responded = None
-            Responded = event.waitKeys(keyList=['f','j','q'])
+            Responded = event.waitKeys(keyList=['f','j','r','q'])
             if Responded[0] == 'f':
                 tag.pop(1)
             elif Responded[0] == 'j':
                 tag.pop(0)
+            elif Responded[0] == 'r':
+                tag = [u'キラキラ', u'ハート', u'音符', u'炎', u'集中線', u'波紋', u'電撃', u'レンズフレア', u'漫画風', u'振動', u'コントラスト', u'拡大', u'縮小']
+                k = len(tag)-1
             elif Responded[0] == 'q':
                 core.quit()
             # 参加者の反応測定終了
+            k = len(tag)-1
 
             # 中視点の準備
             myText = visual.TextStim(myWin,text = '+',pos=(0,0),color = (-1,-1,-1),height=0.2)
